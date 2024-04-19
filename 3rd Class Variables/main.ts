23353641a4ef4727e1b0d1b23fb6c327feca879b
OsamabinAdnan <binadnanosama@gmail.com>
2024-04-19 16:43:20 +0500

    resubmitting with topic names

---
+++ 3rd Class Variables/main.ts
@@ -0,0 +1,36 @@
+//Variable for String/text
+let firstName:string =("Osama bin Adnan");
+let nationality:string = ("Pakistani");
+let message:string = ("I am a programmer");
+let age:string = ("35");
+//when 2 or more string add, it is called Concatenation 
+console.log(firstName+" "+nationality+" "+message+" "+age);
+//we can change value in variable as well
+firstName = "Arham";
+console.log(firstName+nationality+message+age);
+console.log(typeof firstName);
+console.log(typeof nationality);
+console.log(typeof message);
+console.log(typeof age);
+
+
+//variable for number
+let ageNew:number = 5;
+let weight:number = 10;
+let originalNumber:number = 20;
+let value:string = "1";
+console.log(ageNew + weight + originalNumber);
+console.log(ageNew - weight - originalNumber);
+console.log(ageNew * weight * originalNumber);
+console.log((ageNew * weight) / originalNumber);
+console.log(weight ** ageNew);
+console.log(weight % ageNew);
+//when string add with number, number concatenate with string and becomes string
+console.log(ageNew+value);
+
+//Variable for boolean
+let isFeepaid:boolean=true;
+let isMarried:boolean=false;
+console.log(isFeepaid) ;
+console.log(isMarried);
+
---
+++ 4th Class Unary Operators/main.ts
@@ -0,0 +1,6 @@
+let a:number = 6;
+--a;
+++a;
+a--;
+a++;
+console.log(a);
