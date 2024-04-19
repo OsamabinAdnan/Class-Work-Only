750efa67c4a20e545fc7e8439bb47588e3f0bc99
OsamabinAdnan <binadnanosama@gmail.com>
2024-04-19 19:00:35 +0500

    8th Class Functions

---
+++ 8th Class Functions/main.ts
@@ -0,0 +1,58 @@
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>STATIC FUNCTION<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<//
+//declaring function
+function hello(){
+    console.log ('"Hello world from FUNCTION"');
+}
+//calling fucntion
+hello();
+
+//Another program
+function name(){
+    console.log(`"Hello Ali!"`); //static because not everybody has name 'Ali'
+};
+name();
+//Another program
+function halfFryEgg(){
+    let cooked = 1 + 1.5 + 2;
+    console.log(cooked)
+}
+halfFryEgg();
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> RETURNING FUNCTION <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<//
+function halfFryEggs():number{
+    let cooked = 3 + 1.5 + 5;
+    return cooked
+}
+let response:number = halfFryEggs() //invoking the function
+console.log(response);
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>DYNAMIC FUNCTION<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<//
+//Parameter ==> also called function variable ==> dynamic function
+//2 functions should not have same name
+//Syntax function name(parameters){whatever you want to enter in this function write here} call function
+function user_info (name:string, age:number, email:any){
+    console.log(`"My name is ${name}, I am ${age} years old and my email address is ${email}"`)
+}
+user_info ("Osama",35,"osama@gmail.com");
+user_info ("Ali",25,"ali@gmail.com");
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>SQUARE ROOT<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<//
+function square_root(digit:number) {
+    console.log(digit*digit)
+}
+square_root(16);
+square_root(125);
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>CUBE ROOT<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<//
+function cube_root(digit:number) {
+    console.log(digit*digit*digit)
+}
+cube_root(16);
+cube_root(125);
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>ADDITION<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<//
+function addition(digit1:number, digit2:number){
+    console.log(`"The addition of ${digit1} and ${digit2} is ${digit1+digit2}"`)
+}
+addition(45,95);
+addition(145,395);
+addition(97,341);
+
+
+
